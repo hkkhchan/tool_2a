40a9e077aea7e96185c68cf66f3746bd004f14f7
win7 <hkkhchan@gmail.com>
2016-07-30 22:43:25 +0800

    core finish

--- tool_2a.js
+++ tool_2a.js
@@ -1,17 +1,25 @@
 var app= angular.module('toolApp',['ngDialog']).controller('toolCtrl',function($scope,ngDialog){
-	$scope.go=function(){
-
-	}
-	$scope.reset=function(){
-		$scope.layout=1;
-		$scope.method=2;
-		$scope.rows=[{id: 0,show:true,name:'',value:''}];
-		$scope.res_text=$scope.res_html=$scope.res_json='';
+	$scope.src=$scope.target='';
+	$scope.run=function(){
+		if (typeof($scope.src)=='undefined' || typeof($scope.before)=='undefined' || typeof($scope.after)=='undefined') return '';
+		const src=$scope.src;
+		const before=$scope.before;
+		const after=$scope.after;
+		const tl=before.length;
+		var end=0;
+		var arr=new Array();
+		var start=src.indexOf(before,end);
+		while (start>-1 && end> -1){
+			end=src.indexOf(after,start+tl);
+			if (end>-1) arr.push(src.substring(start+tl,end));
+			start=src.indexOf(before,end);
+		}
+		return arr.join(',');
 	}
 	$scope.openDialog= function(){
 		var dialog='<div>';
 		dialog+='<h2>"字串提取"的介紹</h2>';
-		dialog+='<p>本功能主要使用regular expression的技巧，將目標抽取再進行計算。</p>';
+		dialog+='<p>本功能主要使用indexOf來將目標抽取。</p>';
 		dialog+='<p>本頁完全不用JQuery來處理DOM，用AngularJS來將結果呈現出來。</p>';
 		dialog+='<p>AngularJS能給予html資料源作雙向綁定，所以無須綁定元素的事件，使Coding看起來簡潔。</p>';
 		dialog+='</div>';
